e5ed9170f1d8d3a266db5c0fe6f5e5f6b9b3431b
Victor Chan <vicchan95@gmail.com>
2020-03-03 18:50:09 -0500

    Added MapFragment implementation. Added username change implementation. Started TimerTask implementation.

--- app/src/main/java/edu/temple/mapchatapp/MainActivity.java
+++ app/src/main/java/edu/temple/mapchatapp/MainActivity.java
@@ -16,6 +16,9 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -35,6 +38,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.UserLocationInterface {
     private final String getURL = "https://kamorris.com/lab/get_locations.php";
@@ -46,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements ListFragment.User
     LocationListener locationListener;
     Location mlocation;
     RequestQueue queue;
-    ArrayList<Partners> partnersArrayList = new ArrayList();
+    public ArrayList<Partners> partnersArrayList;
 
     private FusedLocationProviderClient fusedLocationProviderClient;
     int minTime = 0, minDistance = 10;
@@ -60,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements ListFragment.User
         setContentView(R.layout.activity_main);
 
         fm = getSupportFragmentManager();
-
+        partnersArrayList = new ArrayList<>();
         queue = Singleton.getInstance(this.getApplicationContext()).getRequestQueue();
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -96,19 +101,56 @@ public class MainActivity extends AppCompatActivity implements ListFragment.User
             getLastKnownLocation();
         }
 
+        // Changing name of user
+        findViewById(R.id.saveButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newUsername = ((EditText)findViewById(R.id.nameBox)).getText().toString();
+                if(!newUsername.isEmpty() && newUsername != null){
+                    setUsername(newUsername);
+//                    postLocation();
+                    Log.d("username changed", username);
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Username cannot be empty",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
         listFrag = fm.findFragmentById(R.id.listContainer);
         // check if fragment is null (first startup)
         if(listFrag == null){
             requestPartners();
+            Log.d("partnersList after method call", partnersArrayList.toString());
         }
         // Not startup, need to update listFrag
         else{
+            // TODO: Finish writing getPartners() to replace requestPartners()
+            //  to get partners from previous state by putting update listFrag in separate method.
+            requestPartners();
             fm.beginTransaction()
                     .remove(listFrag)
                     .add(R.id.listContainer, ListFragment.newInstance(partnersArrayList))
                     .commitAllowingStateLoss();
         }
 
+        mapFrag = fm.findFragmentById(R.id.mapContainer);
+        // Startup
+        if(mapFrag == null){
+            Log.d("before new instance partnersArrayList", partnersArrayList.toString());
+            mapFrag = MapFragment.newInstance(partnersArrayList);
+            fm.beginTransaction()
+                    .add(R.id.mapContainer, mapFrag)
+                    .commitAllowingStateLoss();
+        }
+        // Not startup, replace mapfrag
+        else{
+            // TODO: add method for updating
+            fm.beginTransaction()
+                    .replace(R.id.mapContainer, MapFragment.newInstance(partnersArrayList))
+                    .commitAllowingStateLoss();
+        }
     }
 
     @SuppressLint("MissingPermission")
@@ -123,13 +165,13 @@ public class MainActivity extends AppCompatActivity implements ListFragment.User
                     // permission was granted, yay! Do the
                     // contacts-related task you need to do.
                     getLastKnownLocation();
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            minTime, minDistance, locationListener);
                 } else {
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
                     Log.d("Permissions Result", "Permission denied");
                 }
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime,
-                        minDistance, locationListener);
                 return;
             }
 
@@ -137,7 +179,7 @@ public class MainActivity extends AppCompatActivity implements ListFragment.User
     }
 
     private void postLocation() {
-        StringRequest postRequest = new StringRequest(Request.Method.POST, postURL, new Response.Listener<String>() {
+        StringRequest postRequest = new StringRequest(Request.Method.POST, postURL,new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Log.d("POST Response", response);
@@ -162,55 +204,62 @@ public class MainActivity extends AppCompatActivity implements ListFragment.User
         queue.add(postRequest);
     }
 
-    private void requestPartners() {
-        JsonArrayRequest getRequest = new JsonArrayRequest(Request.Method.GET, getURL,
-                null, new Response.Listener<JSONArray>() {
+    public class requestPartners extends TimerTask
+    {
+        @Override
+        public void run() {
+            JsonArrayRequest getRequest = new JsonArrayRequest(Request.Method.GET, getURL,
+                    null, new Response.Listener<JSONArray>() {
 
-            @Override
-            public void onResponse(JSONArray response) {
-                Log.d("Get Response", response.toString());
-                Log.d("Location", mlocation.toString());
-                partnersArrayList.clear();
-                Partners partner;
-                for(int i = 0; i < response.length(); i++){
-                    try {
-                        JSONObject partnerObject = response.getJSONObject(i);
-                        float[] distance = new float[3];
-                        if(username == partnerObject.getString("username")){
-                            distance[0] = 0;
-                        }
-                        else {
-                            Location.distanceBetween(mlocation.getLatitude(), mlocation.getLongitude(),
+                @Override
+                public void onResponse(JSONArray response) {
+                    Log.d("Get Response", response.toString());
+                    Log.d("Location", mlocation.toString());
+                    partnersArrayList.clear();
+                    Partners partner;
+                    for(int i = 0; i < response.length(); i++){
+                        try {
+                            JSONObject partnerObject = response.getJSONObject(i);
+                            float[] distance = new float[3];
+                            if(username == partnerObject.getString("username")){
+                                distance[0] = 0;
+                            }
+                            else {
+                                Location.distanceBetween(mlocation.getLatitude(), mlocation.getLongitude(),
+                                        Double.valueOf(partnerObject.getString("latitude")),
+                                        Double.valueOf(partnerObject.getString("longitude")), distance);
+                            }
+                            partner = new Partners(partnerObject.getString("username"),
                                     Double.valueOf(partnerObject.getString("latitude")),
-                                    Double.valueOf(partnerObject.getString("longitude")), distance);
+                                    Double.valueOf(partnerObject.getString("longitude")),
+                                    (double) distance[0]);
+                            Log.d("Partner object", "Name: "+partner.getUsername()+
+                                    " distance: " + Double.toString(partner.getDistToUser()));
+                            partnersArrayList.add(partner);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-                        partner = new Partners(partnerObject.getString("username"),
-                                Double.valueOf(partnerObject.getString("latitude")),
-                                Double.valueOf(partnerObject.getString("longitude")),
-                                (double) distance[0]);
-                        Log.d("Partner object", "Name: "+partner.getUsername()+
-                                " distance: " + Double.toString(partner.getDistToUser()));
-                        partnersArrayList.add(partner);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
                     }
+                    Collections.sort(partnersArrayList);
+                    // TODO: remove first value which is user
+                    Log.d("Sorted Array List", "----------------");
+                    for(Partners p : partnersArrayList){
+                        Log.d("Partner Object: ", p.getUsername());
+                    }
+                    listFrag = ListFragment.newInstance(partnersArrayList);
+                    fm.beginTransaction()
+                            .add(R.id.listContainer, listFrag)
+                            .commitAllowingStateLoss();
                 }
-                Collections.sort(partnersArrayList);
-                Log.d("Sorted Array List", partnersArrayList.toString());
-                listFrag = ListFragment.newInstance(partnersArrayList);
-                fm.beginTransaction()
-                        .add(R.id.listContainer, listFrag)
-                        .commitAllowingStateLoss();
-            }
-        }, new Response.ErrorListener() {
+            }, new Response.ErrorListener() {
 
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Get Response", error.toString());
-            }
-        });
-
-        queue.add(getRequest);
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Get Response", error.toString());
+                }
+            });
+            queue.add(getRequest);
+        }
     }
 
     // TODO: Finish writing getLastKnownLocation
@@ -226,24 +275,48 @@ public class MainActivity extends AppCompatActivity implements ListFragment.User
                             Log.d("getLastLocation Response", "Location is not null");
                             mlocation = location;
                             postLocation();
+//                            Log.d("Location after assignment", mlocation.toString());
                             // TODO: Add code to automatically report location update every 30 seconds
+                            Timer timer = new Timer();
+                            TimerTask getRequest = new requestPartners();
+                            timer.schedule(getRequest, 0, 30000);
+
                         } else {
                             Log.d("getLastLocation Response", "Location is null");
                             if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                                /* Request location update RIGHT NOW because last location was null
+                                meaning this was initial startup.
+                                */
                                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                                        minTime, minDistance, locationListener);
+                                        minTime, 0, locationListener);
                             }
                             else{
-                                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                                Log.d("getLastKnownLocation Permission Error", "Permission not granted");
                             }
                         }
                     }
                 });
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                minTime, minDistance, locationListener);
+    }
+
+    private void fragmentDisplay(public ArrayList()){
+
+    }
+
+    private void setUsername(String username){
+        this.username = username;
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        locationManager.removeUpdates(locationListener);
     }
 
     @Override
--- app/src/main/java/edu/temple/mapchatapp/MapFragment.java
+++ app/src/main/java/edu/temple/mapchatapp/MapFragment.java
@@ -4,107 +4,156 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MapFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MapFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MapFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.ArrayList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap gMap;
+    private MapView mapView;
+    private ArrayList<Partners> partnersArrayList;
 
-    private OnFragmentInteractionListener mListener;
+//    private MapFragmentListener mListener;
+
+    private static final String LIST_KEY = "partnerArray";
 
     public MapFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapFragment.
-     */
     // TODO: Rename and change types and number of parameters
-    public static MapFragment newInstance(String param1, String param2) {
+    public static MapFragment newInstance(ArrayList<Partners> partners) {
         MapFragment fragment = new MapFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putParcelableArrayList(LIST_KEY, partners);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        mapView = view.findViewById(R.id.mapView);
+//        if(mapView != null){
+        if(view.findViewById(R.id.mapView) != null){
+            Log.d("mapView return", "MapView is not null");
+            mapView.onCreate(savedInstanceState);
+            mapView.getMapAsync(this);
+        }
+        else{
+            Log.d("mapView return", "mapView is null");
+        }
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        if(googleMap != null) {
+            Log.d("onMapReady partnersArrayList", partnersArrayList.toString());
+            gMap = googleMap;
+            gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            for (int i = 0; i < partnersArrayList.size(); i++) {
+                Partners partner = partnersArrayList.get(i);
+                gMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(partner.getLatitude(), partner.getLongitude()))
+                        .title(partner.getUsername()));
+            }
+        }
+        else{
+            Log.d("onMapReady parameter", "parameter passed in is null");
         }
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            Log.d("getArguments return", "getArguments returns not null");
+            Log.d("getArguments.get return", getArguments().toString());
+            partnersArrayList = getArguments().getParcelableArrayList(LIST_KEY);
+            Log.d("fragment partnersArrayList:", partnersArrayList.toString());
+        }
+        else{
+            Log.d("getArguments return", "getArguments returns null");
         }
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof MapFragmentListener) {
+//            mListener = (MapFragmentListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+
+//    public interface MapFragmentListener{
+//        void onMarkerClick(Marker marker);
+//    }
+
 }
